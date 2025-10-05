d9313120425340cb680463f9f74f1a1ba3f989ea
Muslim-Hassan <muslimhassan772@gmail.com>
2025-10-05 22:12:47 +0430

    By the name of Zainab has been added

--- Program.cs
+++ Program.cs
@@ -10,9 +10,9 @@ namespace SQLiteWithNetCore
             User user = new User()
             {
                 Id = 1,
-                Name = "Hassan",
+                Name = "Zainab",
                 Age = 24,
-                Gender = "Male",
+                Gender = "Female",
                 City = "Kabul",
             };
 
